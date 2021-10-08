6e55769b7518307faf3b701af55f31b1ca99f1ed
JamesBeeProg <JamesBeeProg@gmail.com>
2021-10-08 12:40:54 -0400

    Create menu

---
+++ Assets/Menu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using Photon.Bolt;
+using Photon.Bolt.Matchmaking;
+using UdpKit;
+using TMPro;
+
+public class Menu : GlobalEventListener
+{
+    public TMP_InputField playerName;
+
+    private void Start()
+    {
+        playerName.text = PlayerPrefs.GetString("name", "Player Name");
+    }
+
+    public void OnValueChanged(string name)
+    {
+        PlayerPrefs.SetString("name", name);
+    }
+
+    public void Join()
+    {
+        BoltLauncher.StartClient();
+    }
+
+    public void Host()
+    {
+        BoltLauncher.StartServer();
+    }
+
+    public override void BoltStartDone()
+    {
+        if (BoltNetwork.IsServer)
+        {
+            var matchName = Guid.NewGuid().ToString();
+
+            BoltMatchmaking.CreateSession(
+                sessionID: matchName,
+                sceneToLoad: "Game"
+            );
+        }
+    }
+
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        Debug.LogFormat("Session list updated: {0} total sessions", sessionList.Count);
+
+        foreach (var session in sessionList)
+        {
+            var photonSession = session.Value as UdpSession;
+
+            if (photonSession.Source == UdpSessionSource.Photon)
+            {
+                BoltMatchmaking.JoinSession(photonSession);
+            }
+        }
+    }
+}
---
+++ Assets/NetworkCallbacks.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Bolt;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string scene, IProtocolToken token)
+    {
+        BoltNetwork.Instantiate(BoltPrefabs.Player, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
